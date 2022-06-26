dba43bcdc42753e8c557b22e15e34f865dbf778f
Aleksandr Sysoev <aosysoev@yandex.ru>
2022-06-26 13:53:37 +0300

    db upated

---
+++ modules/db/dataclasses.py
@@ -0,0 +1,55 @@
+###########################
+# файл: dataclass.py
+# version: 0.1.1
+###########################
+
+# Пол пользователя ВКонтакте
+VK_MALE = 2
+VK_FEMALE = 1
+VK_UNKNOWN_GENDER = 0
+
+# Класс определяющий набор данных пользователя ВКонтакте
+class VKUserData(object): 
+    # id пользователя ВКонтакте
+    vk_id : int
+    # Имя пользователя ВКонтакте
+    first_name : str
+    # Фамилия пользователя
+    last_name: str
+    # День рождения пользователя
+    bdate: str
+    # Пол пользоветеля
+    gender : int
+    # id города пользователя
+    city_id: int
+    # Название города пользователя
+    city_title: str
+    # адрес траницы ВКонтакте пользователя
+    vkdomain: str
+    # Дата время последнего общения с ботом
+    last_visit: str
+    # дополнительные свойства пользователя ВКонтакте
+    settings : list
+
+    #инициализация класса
+    def __init__(self):
+        super().__init__()
+
+        # инициализация данных "по умолчанию"
+        self.vk_id = -1
+        self.first_name = ''
+        self.last_name = ''
+        self.bdate = ''
+        self.gender = VK_UNKNOWN_GENDER
+        self.city_id = -1
+        self.city_title = ''
+        self.vkdomain = ''
+        self.last_visit = ''
+        self.settings = {'access_token' : '', 'srch_offset' : -1}
+    # end __init__()
+
+    # вывод данных о пользователе в формате json
+    def json():
+        pass
+
+# end class VKUserData
---
+++ modules/db/db.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE vkusers;
+CREATE USER vkdbadmin WITH PASSWORD 'vk2022boT!!!'
+ALTER DATABASE vkusers OWNER TO vkdbadmin;
+
+CREATE TABLE IF NOT EXISTS vk_users (
+	vk_id INTEGER PRIMARY KEY,
+	first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40),
+    bdate VARCHAR(40),
+    gender INTEGER,
+    city_id INTEGER,
+    city_title VARCHAR(60),
+    vkdomain VARCHAR(100),
+    last_visit VARCHAR(40)
+);
+
+CREATE TABLE IF NOT EXISTS favorites (
+	vk_id INTEGER NOT NULL,
+	fav_id INTEGER NOT NULL,
+	CONSTRAINT favorites_pk PRIMARY KEY (vk_id, fav_id)
+);
+
+CREATE TABLE IF NOT EXISTS black_list (
+	vk_id INTEGER NOT NULL,
+	blk_id INTEGER NOT NULL,
+	CONSTRAINT black_list_pk PRIMARY KEY (vk_id, blk_id)
+);
+
+CREATE TABLE IF NOT EXISTS last_search (
+	vk_id INTEGER NOT NULL,
+	lst_id INTEGER NOT NULL,
+    srch_number INTEGER,
+	CONSTRAINT last_search_pk PRIMARY KEY (vk_id, lst_id)
+);
+
+CREATE TABLE IF NOT EXISTS settings (
+	vk_id INTEGER NOT NULL UNIQUE,
+	access_token VARCHAR (100),
+	srch_offset INTEGER
+);
