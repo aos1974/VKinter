ac77cef1a1ddd26f221485a6395d38c42c620d8f
Aleksandr Sysoev <aosysoev@narod.ru>
2022-07-05 17:08:12 +0300

    databases2.py

---
+++ test_db2.py
@@ -0,0 +1,25 @@
+###########################
+# файл: test_db2.py
+# version: 0.1.1
+# Тестирование работы с БД через databases2.py
+###########################
+
+from modules.utils import utils
+from modules.db.databases import DataBase
+
+# инициализация БД
+#db_name = dbs.POSTGRES_DB
+
+db_name = utils.get_token('db_connection')
+
+if db_name is not None:
+    vk_db = DataBase(db_name)
+else:
+    exit()
+
+# тест запрос к БД на получение данных о пользователе
+vk_id = 37584229
+vk_user = vk_db.get_vkuser(vk_id)
+print(vk_user.vk_id, vk_user.first_name, vk_user.last_name)
+
+
